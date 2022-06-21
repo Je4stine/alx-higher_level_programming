89b6d6f5cbfd554548d3703f708af51901a286ee
Je4stine <je4stine@gmail.com>
2022-06-21 21:21:19 +0300

    Classes corrected

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,25 +4,26 @@ module documentation
 """
 
 
-def __init__(self, size=0):
+class Square:
     """
-    Square documentation
+    Square class
     """
-    
-    if type(size) is not int:
+    def __init__(self, size=0):
+ 
+        if type(size) is not int:
             raise TypeError('size must be an integer')
 
-    if size < 0:
+        if size < 0:
             raise ValueError('size must be >= 0')
 
-    self.size = size
+        self.size = size
 
-@property
-def size(self):
+    @property
+    def size(self):
         return self.__size
 
-@size.setter
-def size(self, size):
+    @size.setter
+    def size(self, size):
         if type(size) is not int:
             raise TypeError('size must be an integer')
 
@@ -31,7 +32,7 @@ def size(self, size):
 
         self.__size = size
 
-def area(self):
+    def area(self):
         """Returns the current square area
 
         """
