81ef6587f25c1b0a1075bcca2e07bc897ae7ac39
Je4stine <je4stine@gmail.com>
2022-07-05 06:51:22 +0100

    Even more corrections

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
 """
-5-base_geometry module
+A integer validator module
 """
 
 
 class BaseGeometry:
     """
-    public method "area"
+    A super class to implements geometrical shapes
     """
+
     def area(self):
-        raise Exception("area() is not implemented")
+        """
+        Raises an exception when you call this function
+        """
+
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{:s} must be an integer".format(name))
+        """
+        Checks a integer value
+
+        Args:
+            name (str): The name of the value.
+            value (int): The value.
+
+        Raises:
+            TypeError: If `value` isn't a integer.
+            ValueError: If `value` is less than or equal to zero.
+        """
+
+        if type(value) is not int:
+            raise TypeError(name + ' must be an integer')
+
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+            raise ValueError(name + ' must be greater than 0')
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,38 +1,32 @@
 #!/usr/bin/python3
 """
-5-base_geometry module
+9-rectangle.py
+a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+add width and height and calculate AREA
 """
 
 
-class BaseGeometry:
-    """
-    public method "area"
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    Rectangle class that extends BaseGeometry class
-    """
+    """create subclass Rectangle of BaseGeometry"""
     def __init__(self, width, height):
-        if super().integer_validator("width", width):
-            self.__width = width
-        if super().integer_validator("height", height):
-            self.__height = height
+        """
+        width and height must be privat
+        and must be positive integer
+        """
+        self.__width = width
+        self.__height = height
 
-    def __str__(self):
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
-
-    def __print__(self):
-        print("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
     def area(self):
-        return self.__height * self.__width
+        """calculate area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """print rectangle width/height"""
+        return '[Rectangle] ' + str(self.__width) + "/" + str(self.__height)
+        
