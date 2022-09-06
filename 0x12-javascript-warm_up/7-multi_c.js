17920fbbf10a80a3aa428b9122d127241a351277
Je4stine <je4stine@gmail.com>
2022-09-06 11:48:13 +0300

    Corrections

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
+if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log('C is fun');
   }
 }
